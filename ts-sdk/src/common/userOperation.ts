ed59e4cee6841f728f051061d345c4ebbd85994d
dylan <yl976931569@163.com>
2024-03-23 19:49:31 +0800

    add recall

---
+++ ts-sdk/src/common/userOperation.ts
@@ -0,0 +1,16 @@
+import type { Address } from "viem"
+import type { Hex } from "viem"
+
+export type UserOperation = {
+    sender: Address
+    nonce: bigint
+    initCode: Hex
+    callData: Hex
+    callGasLimit: bigint
+    verificationGasLimit: bigint
+    preVerificationGas: bigint
+    maxFeePerGas: bigint
+    maxPriorityFeePerGas: bigint
+    paymasterAndData: Hex
+    signature: Hex
+}
